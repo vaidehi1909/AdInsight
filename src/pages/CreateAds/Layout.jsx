75cee9d099e005fc7837509db5093ac5d6526518
v_modi <vaidehi.modi@4ibz.in>
2023-12-30 18:32:00 +0530

    fix UI issue

--- src/pages/CreateAds/Layout.jsx
+++ src/pages/CreateAds/Layout.jsx
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
-import { Grid, Card, CardHeader, CardMedia, CardActions, CardContent, Checkbox, 
-  FormControlLabel, Typography, Button, Box  } from '@mui/material';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { useNavigate } from 'react-router-dom';
+import {
+  Grid,
+  Card,
+  CardHeader,
+  CardMedia,
+  CardActions,
+  CardContent,
+  Checkbox,
+  FormControlLabel,
+  Typography,
+  Button,
+} from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useNavigate } from "react-router-dom";
 
 const Layout = () => {
-
   const [selectedItems, setSelectedItems] = useState([]);
   const navigate = useNavigate();
 
@@ -19,47 +28,46 @@ const Layout = () => {
 
   const onNextClick = () => {
     if (selectedItems.length > 0) {
-      navigate(`/ad/new/${selectedItems.join('_')}`);
+      navigate(`/ad/new/${selectedItems.join("_")}`);
     }
-  }
+  };
 
-    // Create a custom theme with the desired checkbox color
-    const theme = createTheme({
-      components: {
-        MuiCheckbox: {
-          styleOverrides: {
-            root: {
-              color: 'lightgray',
-            },
+  // Create a custom theme with the desired checkbox color
+  const theme = createTheme({
+    components: {
+      MuiCheckbox: {
+        styleOverrides: {
+          root: {
+            color: "lightgray",
           },
         },
       },
-    });
+    },
+  });
 
   return (
-    <Card variant="outlined" sx={{ margin: '0px 10px' }}>
-      <CardHeader 
-        title={
-        <Typography variant="h6" >
-          Create Ads
-        </Typography>
-        }
-      />
+    <Card variant="outlined" sx={{ margin: "0px 10px" }}>
+      <CardHeader title={<Typography variant="h6">Create Ads</Typography>} />
       <CardContent>
         <Grid container spacing={8}>
-          <Grid item xs={12} md={6} sx={{ display: 'flex', justifyContent: 'end' }}>
+          <Grid
+            item
+            xs={12}
+            md={6}
+            sx={{ display: "flex", justifyContent: "end" }}
+          >
             <Card sx={{ maxWidth: 345 }}>
               <CardContent>
                 <ThemeProvider theme={theme}>
                   <FormControlLabel
-                    sx={{ color: 'lightgray' }}
+                    sx={{ color: "lightgray" }}
                     control={
                       <Checkbox
-                        checked={selectedItems.includes('text')}
-                        onChange={() => handleToggleItem('text')}
+                        checked={selectedItems.includes("text")}
+                        onChange={() => handleToggleItem("text")}
                       />
                     }
-                    label=''
+                    label=""
                   />
                 </ThemeProvider>
                 <CardMedia
@@ -68,40 +76,63 @@ const Layout = () => {
                   image="/images/text-ad.png"
                   alt="text-ad"
                 />
-                <Typography variant="body2" color="text.secondary" sx={{ textAlign: 'center', marginTop: '10px' }}>
+                <Typography
+                  variant="body2"
+                  color="text.secondary"
+                  sx={{ textAlign: "center", marginTop: "10px" }}
+                >
                   Create
                 </Typography>
-                <Typography gutterBottom variant="h6" component="div" sx={{ textAlign: 'center' }}>
+                <Typography
+                  gutterBottom
+                  variant="h6"
+                  component="div"
+                  sx={{ textAlign: "center" }}
+                >
                   Text Ad
                 </Typography>
               </CardContent>
             </Card>
           </Grid>
-          <Grid item xs={12} md={6} sx={{ display: 'flex', justifyContent: 'start' }}>
+          <Grid
+            item
+            xs={12}
+            md={6}
+            sx={{ display: "flex", justifyContent: "start" }}
+          >
             <Card sx={{ maxWidth: 345 }}>
               <CardContent>
                 <ThemeProvider theme={theme}>
                   <FormControlLabel
-                    sx={{ color: 'lightgray' }}
+                    sx={{ color: "lightgray" }}
                     control={
                       <Checkbox
-                        checked={selectedItems.includes('media')}
-                        onChange={() => handleToggleItem('media')}
+                        checked={selectedItems.includes("media")}
+                        onChange={() => handleToggleItem("media")}
                       />
                     }
-                    label=''
+                    label=""
                   />
                 </ThemeProvider>
-                 <CardMedia
+                <CardMedia
                   component="img"
                   height="300"
                   image="/images/media-ad.png"
                   alt="media-ad"
                 />
-                <Typography variant="body2" color="text.secondary" sx={{ textAlign: 'center',  marginTop: '10px' }}>
+                <Typography
+                  variant="body2"
+                  color="text.secondary"
+                  sx={{ textAlign: "center", marginTop: "10px" }}
+                >
                   Create
                 </Typography>
-                <Typography gutterBottom variant="h6" component="div" sx={{ textAlign: 'center' }}>
+                <Typography
+                  gutterBottom
+                  variant="h6"
+                  component="div"
+                  sx={{ textAlign: "center" }}
+                >
                   Media Ad
                 </Typography>
               </CardContent>
@@ -109,11 +140,18 @@ const Layout = () => {
           </Grid>
         </Grid>
       </CardContent>
-      <CardActions sx={{ justifyContent: 'flex-end', paddingTop: '100px' }}>
-        <Button variant="contained" size="large" onClick={onNextClick} disabled={selectedItems.length === 0}>Next</Button>
+      <CardActions sx={{ justifyContent: "flex-end", paddingTop: "70px" }}>
+        <Button
+          variant="contained"
+          size="large"
+          onClick={onNextClick}
+          disabled={selectedItems.length === 0}
+        >
+          Next
+        </Button>
       </CardActions>
     </Card>
   );
-}
+};
 
 export default Layout;
